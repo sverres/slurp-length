1ee61edd5c493c92009740b90dbbd935bce97a95
unknown <sverres@L06431.ansatt.hig.no>
2015-06-22 21:05:49 +0200

    Locals with let

--- src/slurp_length/core.clj
+++ src/slurp_length/core.clj
@@ -9,11 +9,15 @@
 
 (defn edgeLength
   [[[v1x v1y] [v2x v2y]]]
-  (Math/sqrt (+ (Math/pow (- v2x v1x) 2) (Math/pow (- v2y v1y) 2))))
+  (let [dx (- v2x v1x)
+        dy (- v2y v1y)
+        d2x (Math/pow dx 2)
+        d2y (Math/pow dy 2)]
+    (Math/sqrt (+ d2x d2y))))
 
 (defn -main
   []
-  (def vertexes (xyzSplit "sykkeltur.csv"))
-  (def edges (mapv vector vertexes (rest vertexes)))
-  (def lineLength (reduce + (mapv edgeLength edges)))
-  (println lineLength))
+  (let [vertexes (xyzSplit "sykkeltur.csv")
+        edges (mapv vector vertexes (rest vertexes))
+        lineLength (reduce + (mapv edgeLength edges))]
+    (println lineLength)))
